f62ba26d0f4292385e9009ef8121c64b17b6164e
charlesassoi <charlesassoi07@gmail.com>
2025-01-21 15:07:12 +0000

    new

---
+++ app.js
@@ -0,0 +1,82 @@
+const computerChoiceDisplay= document.getElementById("computer-choice");
+const userChoiceDisplay= document.getElementById("user-choice");
+const resultDisplay= document.getElementById("result");
+const possibleChoices = document.querySelectorAll("button");
+
+let userChoice;
+let computerChoice;
+let result;
+
+possibleChoices.forEach(possibleChoice=>possibleChoice.addEventListener("click",(e)=>{
+    userChoice=e.target.id;
+    userChoiceDisplay.innerHTML=userChoice;
+    generateComputerChoice();
+    getResult();
+    
+}))
+
+function generateComputerChoice(){
+    const randomNumber=Math.floor(Math.random()*3);
+    if(randomNumber===1){
+        computerChoice= "rock";
+    } else if(randomNumber===2){
+        computerChoice="paper";
+    }else{
+        computerChoice="scissors";
+    }
+    computerChoiceDisplay.innerHTML=computerChoice;
+    console.log(randomNumber);
+    }
+
+function getResult(){
+
+    resultDisplay.innerHTML=result;
+        switch (userChoice){
+            case "rock":
+                switch(computerChoice){
+                    case "rock":
+                        result="draw";
+                        break;
+                    case "paper":
+                        result="lose"
+                        break;
+                    case "scissors":
+                        result="win";
+                        break
+                    default:
+                        result=null;
+                }
+            case "paper":
+                switch(computerChoice){
+                    case "paper":
+                        result="draw";
+                        break;
+                    case "rock":
+                        result="win";
+                        break;
+                    case "scissors":
+                        result="lose";
+                        break;
+                    default:
+                        result=null;
+                }
+            case "scissors":
+                switch(computerChoice){
+                    case "scissors":
+                        result="draw";
+                        break;
+                    case "rock":
+                        result="lose";
+                        break;
+                    case "paper":
+                        result="win";
+                        break;
+                    default:
+                        result=null;
+                }
+
+
+        }
+
+
+    }
